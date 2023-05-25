085aae06915072b7989a92dae1126a9a6957497f
konstantin-d123 <dedovkonstantin732@gmail.com>
2023-05-25 17:35:55 +0700

    User ID и куки

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@ import pandas as pd
 purchases = pd.read_csv('/datasets/returned.csv')
 purchases['total'] = purchases['first'] + purchases['repeated']
 purchases['repeated_share'] = purchases['repeated'] / purchases['total']
-purchases=purchases.sort_values('repeated_share', ascending=False)
+purchases = purchases.sort_values('repeated_share', ascending=False)
 print(purchases.sort_values('repeated_share', ascending=False))
-#print(purchases)
-
+# print(purchases)
+# User ID и куки
