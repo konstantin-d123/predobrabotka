6a6127f132a230ca138508b690fe45c36e3b7f87
konstantin-d123 <dedovkonstantin732@gmail.com>
2023-05-28 12:02:47 +0700

    Вы обнаружили NaN и None 4

--- 1.py
+++ 1.py
@@ -47,3 +47,13 @@ logs = pd.read_csv('/datasets/logs.csv')
 
 visits = logs.groupby('source')['user_id'].count()#общее количество визитов из каждого источника
 print(visits)
+
+import pandas as pd
+
+logs = pd.read_csv('/datasets/logs.csv')
+
+visits = logs.groupby('source')['user_id'].count() # количество визитов
+purchase = logs.groupby('source')['purchase'].sum() # количество покупок
+
+conversion = purchase / visits# поделите количество покупок на количество визитов
+print(conversion)
