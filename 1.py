fef899897f6192277e4dd4fad38904cc0617c3dd
konstantin-d123 <dedovkonstantin732@gmail.com>
2023-06-06 19:53:19 +0700

    Перевод строковых значений в числа 4

--- 1.py
+++ 1.py
@@ -226,3 +226,12 @@ import pandas as pd
 transactions = pd.read_excel('/datasets/ids.xlsx')
 transactions['id'] = pd.to_numeric(transactions['id'], errors='coerce')
 transactions.info()
+
+import pandas as pd
+
+transactions = pd.read_excel('/datasets/ids.xlsx')
+transactions['id'] = pd.to_numeric(transactions['id'], errors='coerce')
+
+transactions['amount'] = pd.to_numeric(transactions['amount'], errors='coerce')
+
+transactions.info()
