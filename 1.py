46c9ded3a23523c7ae4ccbccd62fb5d3a3cebcc0
konstantin-d123 <dedovkonstantin732@gmail.com>
2023-06-08 19:25:53 +0700

    Перевод строковых значений в числа 10

--- 1.py
+++ 1.py
@@ -264,3 +264,9 @@ data = pd.read_excel('/datasets/seo_data.xlsx', sheet_name='traffic_data')
 
 print('Количество строк:', data.shape[0])
 print(data[data['subcategory_id'] == 'total'])
+
+import pandas as pd
+
+data = pd.read_excel('/datasets/seo_data.xlsx', sheet_name='traffic_data')
+data = data[data['subcategory_id'] != 'total']
+print(data[data['subcategory_id'] == 'total'])
