757885bde11f2eeadb63e22e78f9f6613222cfb1
konstantin-d123 <dedovkonstantin732@gmail.com>
2023-06-09 12:23:04 +0700

    Методы Pandas для работы с датой и временем 6

--- 1.py
+++ 1.py
@@ -323,3 +323,13 @@ position['timestamp'] = pd.to_datetime(
 )
 
 print(position.sort_values(by='level', ascending=False))
+
+import pandas as pd
+
+position = pd.read_csv('/datasets/position.csv')
+position['timestamp'] = pd.to_datetime(
+    position['timestamp'], format='%Y-%m-%dT%H:%M:%S'
+)
+
+position['month'] = pd.DatetimeIndex(position['timestamp']).month
+print(position.head())
