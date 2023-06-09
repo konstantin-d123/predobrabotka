7e975a29525cbd4644f28a38909621dc47376380
konstantin-d123 <dedovkonstantin732@gmail.com>
2023-06-09 11:05:29 +0700

    Методы Pandas для работы с датой и временем 3

--- 1.py
+++ 1.py
@@ -297,3 +297,13 @@ import pandas as pd
 position = pd.read_csv('/datasets/position.csv')
 
 position.info()
+
+import pandas as pd
+
+position = pd.read_csv('/datasets/position.csv')
+
+position['timestamp'] = pd.to_datetime(
+    position['timestamp'],
+    format='%Y-%m-%dT%H:%M:%S'
+)
+print(position.head())
