af6ecfb0b42aa5ee06be34f07a5ea107f6e4000b
konstantin-d123 <dedovkonstantin732@gmail.com>
2023-06-09 11:00:25 +0700

    Перевод строковых значений в числа 12

--- 1.py
+++ 1.py
@@ -277,3 +277,10 @@ data = pd.read_excel('/datasets/seo_data.xlsx', sheet_name='traffic_data')
 data = data[(data['subcategory_id'] != 'total')]
 data['visits'] = pd.to_numeric(data['visits']).astype('int')
 data.info()
+
+import pandas as pd
+
+data = pd.read_excel('/datasets/seo_data.xlsx', sheet_name='traffic_data')
+data = data[(data['subcategory_id'] != 'total')]
+data['visits'] = data['visits'].astype('int')
+print(data.groupby('source')['visits'].sum())
