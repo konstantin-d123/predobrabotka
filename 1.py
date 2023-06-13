8b979f5efa4ae9e1a8e2fa6e43a33502a9e0933f
konstantin-d123 <dedovkonstantin732@gmail.com>
2023-06-13 17:05:36 +0700

    Обработка ошибок: try-except 4

--- 1.py
+++ 1.py
@@ -404,3 +404,29 @@ for row in position:
         level = int(row[1]) #в этой переменной сохраните позицию в выдаче
         total_position += level #сложите все позиции в этой переменной
 print(total_position / len(position))
+
+position = [
+    ['2019-05-01', '- 6'],
+    ['2019-05-02', '+5'],
+    ['2019-05-03', ' 5'],
+    ['2019-05-04', '4'],
+    ['2019-05-05', '5'],
+    ['2019-05-06', '5'],
+    ['2019-05-07', '4'],
+    ['2019-05-08', 'Error 5'],
+    ['2019-05-09', '3'],
+    ['2019-05-10', '3'],
+]
+
+count_lines = 0
+wrong_lines_content = []
+
+for row in position:
+    try:
+        count_lines += 1
+        level = int(row[1])
+
+    except:
+        wrong_lines_content.append(row)
+print('Количество измерений', count_lines)
+print('Некорректные строки', wrong_lines_content)
