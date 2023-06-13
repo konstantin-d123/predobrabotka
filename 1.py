10e686023722e22fa97af1488ffdacd3dd807640
konstantin-d123 <dedovkonstantin732@gmail.com>
2023-06-13 17:28:43 +0700

    Метод merge() 1

--- 1.py
+++ 1.py
@@ -430,3 +430,10 @@ for row in position:
         wrong_lines_content.append(row)
 print('Количество измерений', count_lines)
 print('Некорректные строки', wrong_lines_content)
+
+#Метод merge()
+import pandas as pd
+data = pd.read_excel('/datasets/seo_data.xlsx', sheet_name='traffic_data')
+subcategory_dict = pd.read_excel('/datasets/seo_data.xlsx', sheet_name='subcategory_ids')
+data_subcategory = data.merge(subcategory_dict, on='subcategory_id', how='left')
+print(data_subcategory.head(10))
