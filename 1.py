d9312fae7118ca8e8c4a0ea1f989e658a56b17b1
konstantin-d123 <dedovkonstantin732@gmail.com>
2023-06-13 13:37:20 +0700

    Обработка ошибок: try-except 2

--- 1.py
+++ 1.py
@@ -360,3 +360,26 @@ count_lines = 0
 for row in position:
     count_lines =  count_lines+1# допишите ваш код здесь
 print(count_lines)
+
+
+position = [
+    ['2019-05-01', '6'],
+    ['2019-05-02', '5'],
+    ['2019-05-03', '5'],
+    ['2019-05-04', '4'],
+    ['2019-05-05', '5'],
+    ['2019-05-06', '5'],
+    ['2019-05-07', '4'],
+    ['2019-05-08', '4'],
+    ['2019-05-09', '3'],
+    ['2019-05-10', '3'],
+]
+
+count_lines = 0
+total_position = 0
+
+for row in position:
+    count_lines += 1
+    level = int(row[1]) #в этой переменной сохраните позицию в выдаче
+    total_position = total_position + level#сложите все позиции в этой переменной
+print(total_position / count_lines)
