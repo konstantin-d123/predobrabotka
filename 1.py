f284522ea88f4ab0843d1d98117d0c20e1934d13
konstantin-d123 <dedovkonstantin732@gmail.com>
2023-06-16 14:14:46 +0700

    Лаборатория ручного поиска дубликатов 2

--- 1.py
+++ 1.py
@@ -478,3 +478,9 @@ import pandas as pd
 stock = pd.read_excel('/datasets/stock.xlsx', sheet_name='storehouse')
 # ваш код здесь
 print(stock['item'].duplicated().sum())
+
+
+import pandas as pd
+
+stock = pd.read_excel('/datasets/stock.xlsx', sheet_name='storehouse')
+print(stock['item'].value_counts())
