e27e0cb3f7bd1e85e124b424e50ff20582ca4d68
konstantin-d123 <dedovkonstantin732@gmail.com>
2023-06-17 18:02:47 +0700

    Лаборатория ручного поиска дубликатов 5

--- 1.py
+++ 1.py
@@ -484,3 +484,14 @@ import pandas as pd
 
 stock = pd.read_excel('/datasets/stock.xlsx', sheet_name='storehouse')
 print(stock['item'].value_counts())
+
+import pandas as pd
+
+stock = pd.read_excel('/datasets/stock.xlsx', sheet_name='storehouse')
+
+xiaomi = stock[stock['item'] == 'Смартфон Xiaomi Redmi 6A 16GB']['count'].sum()
+huawei = stock[stock['item'] == 'Смартфон HUAWEI P30 lite']['count'].sum()
+
+stock =  stock.drop_duplicates (subset=['item'], keep='first')# удалите строки с дубликатами в столбце 'item'
+# выведите всю таблицу stock на экран
+print(stock)
