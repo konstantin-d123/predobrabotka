34b8c0898dbb399a28f6d5fc9f32077b7784ddee
konstantin-d123 <dedovkonstantin732@gmail.com>
2023-06-18 13:42:26 +0700

    Лаборатория ручного поиска дубликатов 7

--- 1.py
+++ 1.py
@@ -507,3 +507,17 @@ stock = stock.drop_duplicates(subset=['item'], keep='first')
 stock = stock.reset_index(drop=True)  # восстановите индексы с помощью reset_index(drop=True)
 # выведите stock на экран
 print(stock)
+
+import pandas as pd
+
+stock = pd.read_excel('/datasets/stock.xlsx', sheet_name='storehouse')
+xiaomi = stock[stock['item'] == 'Смартфон Xiaomi Redmi 6A 16GB']['count'].sum()
+huawei = stock[stock['item'] == 'Смартфон HUAWEI P30 lite']['count'].sum()
+
+stock = stock.drop_duplicates(subset=['item'], keep='first')
+stock = stock.reset_index(drop=True)
+
+# замените значение в ячейке с количеством смартфонов Xiaomi
+stock.loc[0, 'count'] = xiaomi
+# выведите таблицу stock на экран
+print(stock)
